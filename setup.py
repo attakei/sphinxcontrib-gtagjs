e15d4675f238b27fbbe54ebcb3791b5d8ff59f27
Kazuya Takei <attakei@gmail.com>
2019-12-06 00:43:19 +0900

    Bump 0.1.1

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 """Setup module."""
 from setuptools import setup
 
-setup(version="0.1.0")
+setup(version="0.1.1")
--- sphinxcontrib/gtagjs.py
+++ sphinxcontrib/gtagjs.py
@@ -4,7 +4,7 @@ from jinja2 import Template
 from sphinx.application import Sphinx
 
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 
 def add_gtagjs_context(

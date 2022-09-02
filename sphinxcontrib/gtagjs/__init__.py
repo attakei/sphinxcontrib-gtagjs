0df1e848d1d25d23854bca6757ead4aea3887be2
Kazuya Takei <myself@attakei.net>
2022-09-03 02:35:49 +0900

    feat: Define initialize script for gtag.js as file-template

--- sphinxcontrib/gtagjs/__init__.py
+++ sphinxcontrib/gtagjs/__init__.py
@@ -3,8 +3,7 @@
 Support HTML based builder.
 """
 # flake8: noqa
-from typing import Any
-from jinja2 import Template
+from pathlib import Path
 from sphinx.application import Sphinx
 from sphinx.config import Config
 from sphinx.util import logging
@@ -13,33 +12,7 @@ from sphinx.util import logging
 __version__ = "0.2.1"
 
 logger = logging.getLogger(__name__)
-
-
-def add_gtagjs_context(
-    app: Sphinx, pathname: str, templatename: str, context: dict, doctree: Any
-) -> None:
-    """Build gtag.js tags and register content.
-
-    TODO: Write tests after
-    """
-    if len(app.config.gtagjs_ids) == 0:
-        return
-    template = Template(
-        """
-        <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            {% for gtagjs_id in gtagjs_ids %}
-            gtag('config', '{{ gtagjs_id }}');
-            {% endfor %}
-        </script>
-    """
-    )
-    metatags = context.get("metatags", "") + template.render(
-        gtagjs_ids=app.config.gtagjs_ids
-    )
-    context["metatags"] = metatags
+static_path = Path(__file__).parent / "_static"
 
 
 def resolve_static_files(app: Sphinx, config: Config):
@@ -50,12 +23,18 @@ def resolve_static_files(app: Sphinx, config: Config):
     gtagjs_url = (
         f"https://www.googletagmanager.com/gtag/js?id={app.config.gtagjs_ids[0]}"
     )
-    config.html_js_files.insert(0, gtagjs_url)
+    # NOTE: Now supporting only html or dirhtml
+    # Putting to head of list, for declare gtagjs events by user's js
+    config.html_js_files = [
+        gtagjs_url,
+        "sphinxcontrib.gtagjs.js",
+    ] + config.html_js_files
+    app.config.html_static_path.append(str(static_path))
+    app.config.html_context["gtagjs_ids"] = app.config.gtagjs_ids
 
 
 def setup(app: Sphinx):  # noqa: D103
     app.add_config_value("gtagjs_ids", [], "html")
-    app.connect("html-page-context", add_gtagjs_context)
     app.connect("config-inited", resolve_static_files)
     return {
         "version": __version__,
